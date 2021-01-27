6d2c8fc3f93ecbb1f8d732292584a3399490dcb4
Thiago <thsousa.dev@outlook.com>
2021-01-27 20:22:10 -0300

    Inclusão da página de perguntas

--- pages/index.js
+++ pages/index.js
@@ -4,32 +4,48 @@ import Widget from '../src/components/Widget'
 import Footer from '../src/components/Footer'
 import GitHubCorner from '../src/components/GitHubCorner'
 import QuizBackground from '../src/components/QuizBackground'
+import QuizContainer from '../src/components/QuizContainer'
 import QuizLogo from '../src/components/QuizLogo'
+import { useRouter } from 'next/router'
+import Input from '../src/components/Input'
+import Button from '../src/components/Button'
 
 
-export const QuizContainer = styled.div`
-  width: 100%;
-  max-width: 350px;
-  padding-top: 45px;
-  margin: auto 10%;
-  @media screen and (max-width: 500px) {
-    margin: auto;
-    padding: 15px;
-  }
-`
+
 
 
 export default function Home() {
+  const router = useRouter()
+  const [name, setName] = React.useState('')
+  
+
   return (
     <QuizBackground backgroundImage={db.bg}>
       <QuizContainer>
       <QuizLogo />
         <Widget>
               <Widget.Header>
-                <h1>Teste</h1>
+                <h1>The COVID-19</h1>
               </Widget.Header>
             <Widget.Content>
-              <p>opaopaopaopaoa</p>
+              <form onSubmit={function (event){
+                event.preventDefault()
+                                
+                router.push(`/quiz?name=${name}`)
+                console.log('Fazendo uma submissão por meio do react')
+                
+                }}>
+                <Input 
+                    name = 'nomeDoUsuario'
+                    placeholder="Digite seu nome" 
+                    onChange={ event => setName(event.target.value)}
+                    value = {name}
+                />              
+                
+                <Button type="submit" disabled={name.length === 0}>
+                  {`Jogar ${name}`}
+                </Button>
+              </form>
             </Widget.Content>
         </Widget>
 
